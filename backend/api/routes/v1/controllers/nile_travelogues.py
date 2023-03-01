4562cddbd35594c3b28ca275ab68eab822c780e6
willsu518 <shuang6@uw.edu>
2023-02-28 18:47:07 -0800

    nile-travelogues

--- backend/api/routes/v1/controllers/nile_travelogues.py
+++ backend/api/routes/v1/controllers/nile_travelogues.py
@@ -1,3 +1,21 @@
 # TODO
 # Create the blueprint for nile travelogues. Connect it to the
 # main database blueprint, db_blueprint, in the v1 __init__.py file
+
+
+from flask import Blueprint, jsonify
+from models import cursor
+
+nile_travelogues_blueprint = Blueprint('nile-travelogues', __name__)
+
+@nile_travelogues_blueprint.route("/nile-travelogues")
+def getBoatPassengers():
+    cursor.execute("SELECT * FROM Publications")
+    version = cursor.fetchall()
+    
+    if len(version):
+        columns = [desc[0] for desc in cursor.description]
+        version = [dict(zip(columns, row)) for row in version]
+        return jsonify(version)
+    else:
+        return "No related information"
